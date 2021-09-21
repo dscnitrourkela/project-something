afec6cabf5951582f53690888dea9211555b4e8a
Ritesh Patil <riteshsp2000@gmail.com>
2021-09-21 22:38:57 +0530

    chore: configure gatsby plugins

---
+++ config/website.js
@@ -0,0 +1,33 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'GitWars', // Navigation and Site Title
+  siteTitleAlt: 'May the commits always be with you!', // Alternative Site title for SEO
+  siteTitleShort: 'Gitwars', // short_name for manifest
+  siteUrl: 'https://community.dscnitrourkela.org', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription:
+    'Make a Pull Request to the repo and get featured on the community site of GDSC NIT Rourkela',
+  author: 'DSC NITRourkela', // Author for schemaORGJSONLD
+  organization: 'DSC NITRourkela',
+
+  userTwitter: '@dscnitrourkela', // Twitter Username
+  ogSiteName: 'DSC NITRourkela', // Facebook Site Name
+  ogLanguage: 'en_UK',
+  // googleAnalyticsID: 'G-6KYQ8YWG0J',
+
+  // Manifest and Progress color
+  themeColor: '#11A8FD',
+  backgroundColor: '#353A40',
+
+  // Social component
+  twitterHandle: '@dscnitrourkela',
+  twitter: 'https://twitter.com/dscnitrourkela/',
+  github: 'https://github.com/dscnitrourkela/project-something',
+  linkedin: 'https://www.linkedin.com/company/dscnitrourkela',
+  facebook: 'https://facebook.com/dscnitrourkela',
+  instagram: 'https://instagram.com/dscnitrourkela',
+  discord: 'https://discord.com/invite/SGzuZyp9nS',
+};
