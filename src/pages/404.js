7aaf07b1e9014d16958cceb2f6cc6fc23cec9c5f
Ritesh Patil <riteshsp2000@gmail.com>
2021-09-22 14:11:11 +0530

    refactor: remove default gatsby components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
 
-import Layout from '../components/layout';
-
 const NotFoundPage = () => (
-  <Layout>
+  <div>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </div>
 );
 
 export default NotFoundPage;
