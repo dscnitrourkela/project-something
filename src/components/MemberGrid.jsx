a74a10e825fba642df1a282d99d88a5ed264d054
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-01 11:48:58 +0530

    feat: create home page as per the designs

--- src/components/MemberGrid.jsx
+++ src/components/MemberGrid.jsx
@@ -14,6 +14,14 @@ const BackgroundImageContainer = styled.div`
 
   background: url(https://res.cloudinary.com/dscnitrourkela/image/upload/v1632299484/Gitwars/ixzchacq4xzxyqtoyfct.png);
   object-fit: cover;
+
+  @media (min-width: 1150px) {
+    grid-column: 2/4;
+    grid-row: 1/4;
+    border-radius: 24px;
+    width: 100%;
+    height: 90vh;
+  }
 `;
 
 const GridContainer = styled.div`
@@ -27,6 +35,13 @@ const GridContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+
+  @media (min-width: 1150px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+  }
 `;
 
 const CommunityMemberGrid = () => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,94 @@
-import * as React from 'react';
+import React from 'react';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </div>
-);
+// Libraries
+import styled from 'styled-components';
+
+// Components
+import MemberGrid from '../components/MemberGrid';
+import DesktopNavbar from '../components/marginals/DesktopNavbar';
+import MobileNavbar from '../components/marginals/MobileNavbar';
+
+const PageContainer = styled.div`
+  width: 100%;
+  max-width: 100vw;
+  overflow-x: hidden;
+
+  height: 100vh;
+  overflow-y: hidden;
+  background: linear-gradient(135deg, #32393e, #1d2026);
+`;
+
+const Container = styled.div`
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+  padding: 1rem;
+  overflow-y: hidden;
+`;
+
+const GridContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 1.2rem;
+
+  display: grid;
+  grid-template-columns: 1fr 1.2fr 1.2fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-gap: 3.3rem;
+`;
+
+const ImgContainer = styled.div`
+  grid-column: 1/2;
+  grid-row: 2/3;
+
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 90%;
+  height: auto;
+`;
+
+const MobileViewContainer = styled.div`
+  width: 100%;
+  max-width: 100vw;
+  height: auto;
+  min-height: 100vh;
+  background: linear-gradient(-45deg, #32393e, #1d2026);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IndexPage = () => {
+  const mediaQuery = window.matchMedia('(min-width: 1150px)');
+  console.log(mediaQuery.matches);
+
+  return mediaQuery.matches ? (
+    <PageContainer>
+      <Container>
+        <GridContainer>
+          <DesktopNavbar />
+          <ImgContainer>
+            <Img
+              // eslint-disable-next-line max-len
+              src='https://res.cloudinary.com/dscnitrourkela/image/upload/Gitwars/xm6ww3pkeaj7kys3kvdg.png'
+              alt='Gitlog'
+            />
+          </ImgContainer>
+
+          <MemberGrid />
+        </GridContainer>
+      </Container>
+    </PageContainer>
+  ) : (
+    <>
+      <MobileNavbar />
+      <MobileViewContainer>
+        <MemberGrid />
+      </MobileViewContainer>
+    </>
+  );
+};
 
 export default IndexPage;
